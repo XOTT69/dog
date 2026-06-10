a829a5a0d270d77393e74da9585db25d9fdff938
Антон <mikolenko.anton1@gmail.com>
2026-06-10 22:34:24 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dogcoach-v3';
+const CACHE_VERSION = 'dogcoach-v2';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
@@ -14,7 +14,6 @@ const STATIC_ASSETS = [
   '/js/timer.js',
   '/js/achievements.js',
   '/js/content-loader.js',
-  '/js/offline-queue.js',
   '/manifest.webmanifest',
   '/assets/icon-192.png',
   '/assets/icon-512.png',
